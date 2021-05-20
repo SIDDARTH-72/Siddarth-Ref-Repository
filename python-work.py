87fd1efc2b5fb89ec0fcf2bc68add1c093b69039
Siddarth72 <72@siddarth.com>
2021-05-20 14:07:56 +0530

    py all

--- python-work.py
+++ python-work.py
@@ -1,1 +1,34 @@
 print('PY')
+# nums = [1, 2, 3, 4]
+# nums.insert(4, 7) this is index value and object
+# nums.remove(2) this is not the index value, this the object that
+# print(nums)
+# tuple is written in round brackets
+# set is written in curly brackets
+
+# type casting -- changing the datatype.
+# a = 5.6
+# b = int(a)
+# print(b)
+
+# print(int(True)) = 1
+# print(int(False)) = 0
+
+# str = "Siddarth"
+# print(len(str)) = 8
+
+# print(list(range(1, 10, 2)))
+# first number is included but last number is not included. 3rd argument is the common difference.
+
+# mapping--
+# dick = {'java': 'eclipse', 'c prog':'codeblocks', 'python':'pycharm' }
+# print(dick)
+# print(dick.keys())
+# print(dick.values())
+# print(dick['java'])
+
+# arithmetic operators: '+','-','*','/','%' % is used to get the remainder.
+# assignment operator:
+# n = 2
+# n = -n
+# print(n) = -2
